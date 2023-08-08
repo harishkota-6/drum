f6bf32b9d4f14b04c43ad859780991dc94cd0de8
Harish <harishkota01@gmail.com>
2023-08-08 15:27:43 +0530

    commit-1

---
+++ index.js
@@ -0,0 +1,68 @@
+var l = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<l; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+// document.querySelectorAll
+function handleClick(){
+    
+    var butVal = this.innerHTML;
+    PlaySound(butVal);
+    addAnimation(butVal);
+    // alert(butVal);
+    
+}
+
+document.addEventListener("keypress", function (event){
+    PlaySound(event.key);
+}
+);
+
+
+
+function PlaySound(key) {
+    
+    switch (key) {
+        case ("w"):
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case ("a"):
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case ("s"):
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case ("d"):
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;        
+        case ("j"):
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case ("k"):
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case ("l"):
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+
+}
+
+function addAnimation(key){
+
+    document.querySelector("."+key).classList.add("pressed");
+
+    setTimeout(function (){
+        document.querySelector("."+key).classList.remove("pressed");
+    },150 );
+}
